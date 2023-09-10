3231ec65e1fcac8e6b70b1dbc5b6ec41831cf46a
weiyang-weike <119039658+weiyang-weike@users.noreply.github.com>
2023-09-10 22:33:10 +0800

    Docking done

--- VzApplicationCore/src/VizzioCore/Application.cpp
+++ VzApplicationCore/src/VizzioCore/Application.cpp
@@ -80,6 +80,63 @@ namespace Vizzio {
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
+			{
+				static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+				// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+				// because it would be confusing to have two docking targets within each others.
+				ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDocking;
+				/*if (m_MenubarCallback)
+					window_flags |= ImGuiWindowFlags_MenuBar;*/
+
+				const ImGuiViewport* viewport = ImGui::GetMainViewport();
+				ImGui::SetNextWindowPos(viewport->WorkPos);
+				ImGui::SetNextWindowSize(viewport->WorkSize);
+				ImGui::SetNextWindowViewport(viewport->ID);
+				ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+				ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+				window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+				window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+				// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+				// and handle the pass-thru hole, so we ask Begin() to not render a background.
+				if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+					window_flags |= ImGuiWindowFlags_NoBackground;
+
+				// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+				// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+				// all active windows docked into it will lose their parent and become undocked.
+				// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+				// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+				ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+				ImGui::Begin("DockSpace Demo", nullptr, window_flags);
+				ImGui::PopStyleVar();
+
+				ImGui::PopStyleVar(2);
+
+				// Submit the DockSpace
+				ImGuiIO& io = ImGui::GetIO();
+				if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+				{
+					ImGuiID dockspace_id = ImGui::GetID("VulkanAppDockspace");
+					ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+				}
+
+				/*if (m_MenubarCallback)
+				{
+					if (ImGui::BeginMenuBar())
+					{
+						m_MenubarCallback();
+						ImGui::EndMenuBar();
+					}
+				}*/
+
+				for (auto& layer : m_LayerStack)
+					layer->OnUIRender();
+
+				ImGui::End();
+			}
+
 			// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
 			if (show_demo_window)
 				ImGui::ShowDemoWindow(&show_demo_window);
@@ -122,11 +179,15 @@ namespace Vizzio {
 			int display_w, display_h;
 			glfwGetFramebufferSize(m_WindowHandle, &display_w, &display_h); 
 
+			glViewport(0, 0, display_w, display_h);
+			glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+			glClear(GL_COLOR_BUFFER_BIT);
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-			for (auto& layer : m_LayerStack)
-			{
-				layer->OnUIRender();
+			
+			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) 
+			{ 
+				ImGui::UpdatePlatformWindows();
+				ImGui::RenderPlatformWindowsDefault(); 
 			}
 
 			glfwSwapBuffers(m_WindowHandle);
@@ -165,10 +226,10 @@ namespace Vizzio {
 #else
 	// GL 3.0 + GLSL 130
 		const char* glsl_version = "#version 130";
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-		//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE); 
 #endif
 
 		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -187,17 +248,40 @@ namespace Vizzio {
 			return;
 		}
 
-		glfwSwapInterval(1); // Enable vsync
+		glfwSetInputMode(m_WindowHandle, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+		glfwMakeContextCurrent(m_WindowHandle);
+
+		glfwSwapInterval(1);
+		/*	glfwSetMouseButtonCallback(m_WindowHandle, MouseClickCallback);
+			glfwSetCharCallback(m_WindowHandle, CharCallback);
+			glfwSetCursorPosCallback(m_WindowHandle, MouseMoveCallback);
+			glfwSetErrorCallback(ErrorCallback);
+			glfwSetJoystickCallback(JoystickCallback);
+			glfwSetKeyCallback(m_WindowHandle, KeyCallback);
+			glfwSetScrollCallback(m_WindowHandle, ScrollCallback);
+			glfwSetWindowSizeCallback(m_WindowHandle, WinResizeCallback);
+			glfwSetInputMode(m_WindowHandle, GLFW_CURSOR, GLFW_CURSOR_NORMAL);*/
+
 		// Setup Dear ImGui context
 
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 		// Setup Dear ImGui style
 		ImGui::StyleColorsDark();
 		//ImGui::StyleColorsLight();
+		// 
+		// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+		ImGuiStyle& style = ImGui::GetStyle();
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			style.WindowRounding = 0.0f;
+			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+		}
 
 		// Setup Platform/Renderer backends
 		ImGui_ImplGlfw_InitForOpenGL(m_WindowHandle, true); 
