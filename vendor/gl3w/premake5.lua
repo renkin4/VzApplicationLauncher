2650c9ae57db7181ac77a968745470bd4272cf5d
weiyang-weike <119039658+weiyang-weike@users.noreply.github.com>
2023-09-10 21:52:43 +0800

    added gl3w

---
+++ vendor/gl3w/premake5.lua
@@ -0,0 +1,22 @@
+project "Gl3w"
+    kind "StaticLib"
+    language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+ 	files
+	{
+        "include/gl/gl3w.h",
+        "include/KHR/khrplatform.h",
+        "src/gl3w.c"
+    }
+
+	includedirs
+	{
+		"include"
+	}
+
+ 	filter "system:windows" 
+        systemversion "latest"
+        staticruntime "On"
